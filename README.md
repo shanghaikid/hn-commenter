89bc118cba78a767984dd193904e52776023163a
shanghaikid <jiangruiyi@gmail.com>
2023-04-07 19:44:24 +0800

    update extension, support save data on click extension icon
    Signed-off-by: shanghaikid <jiangruiyi@gmail.com>

---
+++ README.md
@@ -0,0 +1,10 @@
+# HN-commenter
+
+Chrome extension that will help you to reply hn thread.
+
+## TODO
+
+- [x] support settting api-key
+- [] support setting model
+- [] support setting system role
+- [] support setting instruction
--- content.js
+++ content.js
@@ -1,16 +1,27 @@
 chrome.storage.sync.get("apiKey", function (data) {
-  let apiKey = data.apiKey;
+  const apiKey = data.apiKey;
+  const buttonText = `Get reply from ChatGPT`;
+  const model = `gpt-3.5-turbo`;
+  const systemRole = `As a software engineer in Silicon Valley`;
+  const instruction = ``;
+
+  // filter
+  function filterText(text) {
+    return text.replaceAll(buttonText, "");
+  }
+
   // Create a function to extract the comments and generate a response
   function generateResponse(button) {
     // Get the story title
-    let title = document.querySelector(".title a").innerText;
+    const title = document.querySelector(".title a").innerText;
+    const top = { role: "system", content: title };
 
     // Get the top-level comment elements
-    let rows = Array.from(
+    const rows = Array.from(
       document.querySelectorAll(".comment-tree .athing.comtr")
     );
 
-    const comments = [];
+    const messages = [];
 
     let started = false;
 
@@ -24,42 +35,62 @@ chrome.storage.sync.get("apiKey", function (data) {
         if (!row.querySelector(".commtext.c00")) continue;
 
         const comment = {
-          content: row.querySelector(".commtext.c00").innerText,
+          role: "user",
+          content: filterText(row.querySelector(".commtext.c00").innerText),
           level: Number(indentLevel),
         };
 
-        if (comments.length === 0 || comment.level < comments[0].level) {
-          comments.unshift(comment);
+        if (messages.length === 0 || comment.level < messages[0].level) {
+          messages.unshift(comment);
         }
       }
     }
 
-    // Combine the comments into a single string
-    let commentText = comments.map((comment) => comment.content).join("\n\n");
-    console.log(commentText);
+    messages.forEach((m) => {
+      delete m.level;
+    });
+
+    // add to top
+    messages.unshift(top);
+
+    // Combine the messages into a single string
+    // let commentText = messages.map((comment) => comment.content).join("\n\n");
+    console.log(messages);
+
+    // start fetching
+    button.innerText = `Generating reply ...`;
     // Generate a response using the ChatGPT API
-    fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+    fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
-        prompt: `Topic: ${title}\n\nComments: ${commentText}`,
-        max_tokens: 1024,
-        temperature: 0.5,
-        n: 1,
+        model: model,
+        messages: messages,
+        temperature: 0.7,
       }),
     })
       .then((response) => response.json())
       .then((data) => {
         // Display the response to the user
-        let responseText = data.choices[0].text;
-        alert(responseText);
+        const responseText = data.choices[0].message.content;
+        // create a copy button
+        const copyBtn = document.createElement("button");
+        copyBtn.className = "copyButton";
+        copyBtn.innerText = "Copy";
+        copyBtn.setAttribute("data-reply", responseText);
+        // put it beside the reply
+        button.insertAdjacentElement("afterend", copyBtn);
+        console.log(responseText);
       })
       .catch((error) => {
         console.error(error);
         alert("Error generating response. Please try again later.");
+      })
+      .finally(() => {
+        button.innerText = buttonText;
       });
   }
 
@@ -67,9 +98,18 @@ chrome.storage.sync.get("apiKey", function (data) {
   let replyLinks = document.querySelectorAll(".reply a");
   replyLinks.forEach((link) => {
     let button = document.createElement("button");
-    button.innerText = "ChatGPT Reply";
+    button.innerText = buttonText;
     button.style.marginLeft = "10px";
     button.addEventListener("click", () => generateResponse(button));
     link.parentNode.insertBefore(button, link.nextElementSibling);
+  });
+
+  // Listen for a click event on any of the buttons
+  document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("copyButton")) {
+      const postTitle = event.target.getAttribute("data-reply");
+      navigator.clipboard.writeText(postTitle);
+      alert("copied");
+    }
   });
 });
